bc7478381a3a86dd9df840cc5c67273b28df7f16
Benny Franco <bennyfranco@icloud.com>
2019-10-01 21:13:29 -0700

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    menu,
+    inGame,
+    gameOver
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameState currentGameState = GameState.menu;
+
+    public static GameManager sharedInstance;
+
+    private PlayerController controller;
+
+    void Awake()
+    {
+        if(sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Submit") && currentGameState != GameState.inGame)
+        {
+            StartGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        SetGameState(GameState.inGame);
+    }
+
+    public void GameOver()
+    {
+        SetGameState(GameState.gameOver);
+    }
+
+    public void BackToMenu()
+    {
+        SetGameState(GameState.menu);
+    }
+
+    private void SetGameState(GameState newGameState)
+    {
+        if(newGameState == GameState.menu)
+        {
+            // TODO: Set game menu logic here.
+        }
+        else if(newGameState == GameState.inGame)
+        {
+            LevelManager.sharedInstance.RemoveAllLevelBlocks();
+            LevelManager.sharedInstance.GenerateInitialBlocks();
+            controller.StartGame();
+        }
+        else if (newGameState == GameState.gameOver)
+        {
+            // TODO: Game over logic here.
+        }
+
+        currentGameState = newGameState;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 6f;
+    public float runningSpeed = 2f;
+
+    public LayerMask groundMask;
+
+    private Rigidbody2D playerRigidbody;
+    private Animator animator;
+    private Vector3 startPosition;
+
+    private const string STATE_ALIVE = "isAlive";
+    private const string STATE_ON_THE_GROUND = "isOnTheGround";
+
+    void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    public void StartGame()
+    {
+        animator.SetBool(STATE_ALIVE, true);
+        animator.SetBool(STATE_ON_THE_GROUND, true);
+        Invoke("RestartPosition", 0.1f);
+    }
+
+    void RestartPosition()
+    {
+        transform.position = startPosition;
+        playerRigidbody.velocity = Vector2.zero;
+        GameObject camera = GameObject.Find("Main Camera");
+        camera.GetComponent<CameraFollow>().ResetCameraPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isTouching = IsTouchingTheGround();
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isTouching)
+            {
+                Jump();
+            }
+        }
+
+        Debug.DrawRay(transform.position, Vector2.down * 1.5f, Color.red);
+
+        animator.SetBool(STATE_ON_THE_GROUND, isTouching);
+    }
+
+    void FixedUpdate()
+    {
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+            if(playerRigidbody.velocity.x < runningSpeed)
+            {
+                playerRigidbody.velocity = new Vector2(runningSpeed, playerRigidbody.velocity.y);
+            }
+        }
+        else
+        {
+            playerRigidbody.velocity = new Vector2(0, playerRigidbody.velocity.y);
+        }
+    }
+
+    void Jump()
+    {
+        if (GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+            playerRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    bool IsTouchingTheGround()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, 1.5f, groundMask);
+    }
+
+    public void Die()
+    {
+        animator.SetBool(STATE_ALIVE, false);
+        GameManager.sharedInstance.GameOver();
+    }
+}
